086b9efd9f627986ef849a86e1ee8f5f8eecf68d
Matt Chadburn <matt@commuterjoy.co.uk>
2012-08-05 13:48:35 +0100

    Two implementations with passing tests

--- src/main/scala/Homework.scala
+++ src/main/scala/Homework.scala
@@ -1,8 +1,18 @@
 object Homework {
   def foo = "foo"
-
-  ////////////////////////////////////
-
+  
   val naughtyWords = List("Celery", "Megatron")
-  def isNaughty(text: String) = false
+
+  // attempt #1 - one liner
+  def xisNaughty(text: String) : Boolean = {
+    naughtyWords.map(s => text.toLowerCase.contains(s.toLowerCase)).contains(true)
+  }
+
+  // attempt #2 - case insensitive regex 
+  def isNaughty(text: String) : Boolean = {
+      naughtyWords.filter { case s: String =>
+        (("(?i)" + s).r findFirstMatchIn text) != None
+      }.length > 0
+    }
+
 }
